f6dcfe8015088c9fe9c2ab1d3f7152ac4ec72efe
framedr0p <m@arkus.rocks>
2021-08-06 21:46:36 +0200

    added esp32.c

---
+++ esp32.c
@@ -0,0 +1,78 @@
+#include <WiFi.h>
+
+#define echoPin 6     // Echo HC-SR04
+#define trigerPin 7   // Trigger HC-SR04
+
+long  id;             // ID des Sensors
+long  id1000;         // abgewandelte ID des Sensors wenn freier PP
+long  distance;       // gemessener Abstand
+long  distancelimit;  // ab welchem Abstand gilt der Parkplatz als frei
+long duration;        // vom Sensor gemessene Dauer
+long distance;        // aus Dauer errechnete Entfernung
+
+String  request;
+IPAddress staticIP709_10.10.0.99(10.10.0.99);
+IPAddress gateway709_10.10.0.99(10.10.0.254);
+IPAddress subnet709_10.10.0.99(255.255.255.0);
+
+WiFiClient client;
+
+String myurl = "/";
+String line="";
+String IotClientSendWithAnswer(String IPcache, String monmessagecache) {
+line="";
+client.print(String("GET ") + myurl +monmessagecache + " HTTP/1.1\r\n" +
+          "Host: " + IPcache + "\r\n" +
+         "Connection: close\r\n\r\n");
+unsigned long timeout = millis();
+while (client.available() == 0) {
+if (millis() - timeout > 5000) {
+client.stop();
+return "Client Timeout!";
+}
+}
+while(client.available()) {line += client.readStringUntil('\r');}
+return line;
+}
+
+void setup()
+{
+id = 101;               // ID des Sensors
+id1000 = 1101;          // ID des Sonsors wenn PP frei
+distance = 0;           // gemessener Abstand
+distancelimit = 100000; // ab welchem Abstand gilt der Parkplatz als frei
+request = "";
+
+Serial.begin(9600);
+
+  WiFi.disconnect();
+  delay(3000);
+  Serial.println("START");
+  WiFi.begin("HOME","8QU5TCD2V7DSBP4MZNZQV");     // WiFi Zugangsdaten
+  while ((!(WiFi.status() == WL_CONNECTED))){
+    delay(300);
+    Serial.print("..");
+
+  }
+  Serial.println("Connected");
+  WiFi.config(staticIP709_10.10.0.99, gateway709_10.10.0.99, subnet709_10.10.0.99);
+  Serial.println("Your IP is");
+  Serial.println((WiFi.localIP()));
+
+}
+
+
+void loop()
+{
+
+    if (client.connect("10.10.0.98", 80)) {
+      if (distance < distancelimit) {
+        request = id1000;
+
+      }
+      request = id;
+      Serial.println((IotClientSendWithAnswer("10.10.0.98",request)));
+
+    }
+
+}
