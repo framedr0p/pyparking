fe68a3447c923dc129f45589705cfbbb8932bdf1
framedr0p <m@arkus.rocks>
2021-08-06 22:13:01 +0200

    Abstandsmessung Grundlagen eingefügt

--- esp32.c
+++ esp32.c
@@ -1,12 +1,11 @@
 #include <WiFi.h>
-
+#define ledPin 5      // LED
 #define echoPin 6     // Echo HC-SR04
 #define trigerPin 7   // Trigger HC-SR04
 
-long  id;             // ID des Sensors
-long  id1000;         // abgewandelte ID des Sensors wenn freier PP
-long  distance;       // gemessener Abstand
-long  distancelimit;  // ab welchem Abstand gilt der Parkplatz als frei
+long id;              // ID des Sensors
+long id1000;          // abgewandelte ID des Sensors wenn freier PP
+long distancelimit;   // ab welchem Abstand gilt der Parkplatz als frei
 long duration;        // vom Sensor gemessene Dauer
 long distance;        // aus Dauer errechnete Entfernung
 
@@ -39,9 +38,11 @@ void setup()
 {
 id = 101;               // ID des Sensors
 id1000 = 1101;          // ID des Sonsors wenn PP frei
-distance = 0;           // gemessener Abstand
 distancelimit = 100000; // ab welchem Abstand gilt der Parkplatz als frei
 request = "";
+
+pinMode(echoPin, INPUT); 
+pinMode(trigPin, OUTPUT);
 
 Serial.begin(9600);
 
@@ -64,15 +65,38 @@ Serial.begin(9600);
 
 void loop()
 {
+  // Sensor wird von HIGH Puls ausgelöst, der länger als 10 Microsekunden dauert
+  // vorher ein kurzer LOW Puls, für sauberen HIGH Puls
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
 
-    if (client.connect("10.10.0.98", 80)) {
-      if (distance < distancelimit) {
-        request = id1000;
+  // Signal vom Sensor einlesen:
+  // duration = Zeit in Mikrosekunden für Senden eines Pings
+  // bis zum Empfangen des Echos von einem Objekt
+  duration = pulseIn(echoPin, HIGH);
 
-      }
+  // Formel: distance = 1/2 * duration / Schallgeschwindigkeit
+  // Schallgeschwindigkeit ca 323m/s
+  // durch 2, da duration die Zeit für hin und zurück ist
+  distance = duration * 0.034 / 2;
+
+  // Entkommentieren, falls gemessener Abstand auf Serial Monitor ausgegeben werden soll
+  // Serial.print("Abstand: ");
+  // Serial.println(distance);
+
+  if (client.connect("10.10.0.98", 80)) {
+    if (distance <= distancelimit) {    // falls Abstand zu klein -> Parkplatz belegt
       request = id;
-      Serial.println((IotClientSendWithAnswer("10.10.0.98",request)));
-
     }
-
+    // sonst ist der Parkplatz frei
+    else if (distance > distancelimit) {
+      request = id1000;
+    }
+    // sende den Request an den Controller
+    Serial.println((IotClientSendWithAnswer("10.10.0.98",request)));
+  }
+  delay(100);
 }
